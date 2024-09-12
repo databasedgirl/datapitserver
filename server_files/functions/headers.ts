f2297b46885a341bad46bb162e73e47ef9f4265e
Ana <ana.narley@gmail.com>
2024-09-12 20:01:52 -0300

    Delete server_files directory

--- server_files/functions/headers.ts
+++
@@ -1,8 +0,0 @@
-import {Request, Response, NextFunction} from 'express';
-
-export class Headers{
-    cors_ACAO(req:Request,res:Response,next:NextFunction): void{
-        res.header('Access-Control-Allow-Origin',process.env.FRONT_HOST);
-        next();
-    }
-}
