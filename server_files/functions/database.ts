807911edf4b34c4c9800681622ec117ef6dfecce
Ana <ana.narley@gmail.com>
2024-09-12 22:50:06 -0300

    Update database.ts

--- server_files/functions/database.ts
+++ server_files/functions/database.ts
@@ -1,14 +1,25 @@
-import {Connection, createConnection} from 'mysql';
+import { createConnection, Connection} from 'mysql2';
 import dotenv from 'dotenv';
+import { notify } from './functions';
+import fs from 'fs';
 
 dotenv.config();
 export async function connect(){
-    var conn:Connection = createConnection({
-        host:process.env.DB_HOST,
-        port:3306,
-        database:process.env.DB,
-        user:process.env.DB_USER,
-        password:process.env.DB_PASS,
-    });
-    return conn;    
+    try{
+        var conn:Connection = createConnection({
+            host:process.env.DB_HOST,
+            port:20914,
+            database:process.env.DB,
+            user:process.env.DB_USER,
+            password:process.env.DB_PASS,
+            ssl:{
+                ca:fs.readFileSync(__dirname+process.env.CA_FILE)
+            }
+        });
+        return conn; 
+    }catch(err){
+        notify(`DBERR: ${err.stack}\nERRNO: ${err.errno}`,1);
+        return false;
+    }
+       
 }
